815d4288081967266eb66d52850fe4b24ff5e6f9
Yakov <yakjunior78@gmail.com>
2022-04-25 00:26:14 +0300

    refactor watch function to render dynamic layouts based on isLoggedIn state

--- iprocure/src/components/layouts/index.vue
+++ iprocure/src/components/layouts/index.vue
@@ -1,26 +1,25 @@
 <script setup lang="ts">
 import DefaultLayout from "./default.vue";
 import GuestLayout from "./guest.vue";
-import { markRaw, ref, watch } from "vue";
-import { useRoute } from "vue-router";
+import {markRaw, ref, watch} from "vue";
 import store from "@/store";
 
 const layout = ref();
-const route = useRoute();
 
 watch(
-  () => route.meta?.layout as string | undefined,
-  async () => {
-    layout.value = store.getters.isAuthenticated
-      ? markRaw(DefaultLayout)
-      : markRaw(GuestLayout);
-  },
-  { immediate: true }
+	() => store.getters.isAuthenticated as string | undefined,
+	async () => {
+		console.log('IS LOGGED IN  : ', store.getters.isAuthenticated);
+		layout.value = store.getters.isAuthenticated
+			? markRaw(DefaultLayout)
+			: markRaw(GuestLayout);
+	},
+	{immediate: true}
 );
 </script>
 
 <template>
-  <component :is="layout">
-    <router-view />
-  </component>
+	<component :is="layout">
+		<router-view/>
+	</component>
 </template>
